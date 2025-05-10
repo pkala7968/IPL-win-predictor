9036c10f158995933ebcae7ac1bbd2343217db35
pkala7968 <pratyushkala.79@gmail.com>
2025-05-10 20:02:00 +0530

    minor changes

--- app.py
+++ app.py
@@ -183,4 +183,5 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=os.environ.get("FLASK_DEBUG", "0") == "1")
